51a87907b307dca78db76491238da9aaa4bc4c3a
Varshana01 <91725838+Varshana01@users.noreply.github.com>
2021-11-21 22:43:56 +0400

    Week4 Day5

---
+++ MiniProject/DailyDay5.py
@@ -0,0 +1,7 @@
+# #DailyChallenge Day5
+# def sort_words():
+#     items=input("Enter a sequence of words separated with commas: ")
+#     words = [word for word in items.split(",")]
+#     print(",".join(sorted(list(words))))
+#
+# sort_words()
